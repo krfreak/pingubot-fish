883a4e98ad974f8d504721b7c7fb3da94f8cc6ab
Tim Schnell <kreedzfreak@gmail.com>
2017-04-08 04:41:45 +0200

    Update mtg_card_fetcher.rb

--- fish/mtg_card_fetcher.rb
+++ fish/mtg_card_fetcher.rb
@@ -8,29 +8,40 @@ module Bot
       puts "MTG Card Fetcher loaded"
       command :card do |event, *args|
         a = Concurrent::Promise.fulfill(MTG::Card.where(name: "#{args.join(' ')}").all.first).then{|result|
-          begin
-            event.channel.send_embed do |embed|
-              embed.title = "#{result.name}"
-              embed.colour = 0x14506f
-              embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{result.image_url}")
-              embed.add_field(name: "Colour:", value: result.colors.reject(&:empty?).join(', '))
-              embed.add_field(name: "Mana cost:", value: result.mana_cost)
-              embed.add_field(name: "Type line:", value: result.type)
-              embed.add_field(name: "Types:", value: result.types.reject(&:empty?).join(', '))
-              embed.add_field(name: "Rulings:", value: result.text)
-              embed.add_field(name: "Power/Toughness:", value: "#{result.power}/#{result.toughness}") if result.types.include?("Creature")
+          if result.image_url.nil?
+            begin
+              event.channel.send_embed do |embed|
+                embed.title = "#{result.name}"
+                embed.colour = 0x14506f
+                embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{result.image_url}")
+                embed.add_field(name: "Colour:", value: result.colors.reject(&:empty?).join(', '))
+                embed.add_field(name: "Mana cost:", value: result.mana_cost)
+                embed.add_field(name: "Type line:", value: result.type)
+                embed.add_field(name: "Types:", value: result.types.reject(&:empty?).join(', '))
+                embed.add_field(name: "Rulings:", value: result.text)
+                embed.add_field(name: "Power/Toughness:", value: "#{result.power}/#{result.toughness}") if result.types.include?("Creature")
+              end
+            rescue Discordrb::Errors::NoPermission
+              text = ""
+              text << "**Colour:** " << result.colors.reject(&:empty?).join(', ')
+              text << "**Mana cost:** " << result.mana_cost
+              text << "**Type line:**" <<  result.type
+              text << "**Types:**" <<  result.types.reject(&:empty?).join(', ')
+              text << "**Rulings:**" <<  result.text
+              if result.types.include?("Creature")
+                text << "**Power/Toughness:**" << "#{result.power}/#{result.toughness}"
+              end
+              event.channel.send_message "#{text}"
             end
-          rescue Discordrb::Errors::NoPermission
-            text = ""
-            text << "**Colour:** " << result.colors.reject(&:empty?).join(', ')
-            text << "**Mana cost:** " << result.mana_cost
-            text << "**Type line:**" <<  result.type
-            text << "**Types:**" <<  result.types.reject(&:empty?).join(', ')
-            text << "**Rulings:**" <<  result.text
-            if result.types.include?("Creature")
-              text << "**Power/Toughness:**" << "#{result.power}/#{result.toughness}"
+          else
+            begin
+              event.channel.send_embed do |embed|
+                embed.colour = 0x14506f
+                embed.image = Discordrb::Webhooks::EmbedImage.new(url: "#{result.image_url}")
+              end
+            rescue Discordrb::Errors::NoPermission
+              event.channel.send_message "#{result.image_url}"
             end
-            event.channel.send_message "#{text}"
           end
         }
         a.execute.rescue{|reason|
